8eaacff6ad0b5bf7f8f950b51258c30fa33fa205
RonyPony <ronel.cruz.a8@gmail.com>
2022-08-16 11:26:02 -0400

    adding swagger to production

--- vendio-backend/Program.cs
+++ vendio-backend/Program.cs
@@ -1,9 +1,17 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.EntityFrameworkCore;
+using vendio_backend.Models;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<vendionContext>(opt =>
+{
+    //opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnections"));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("postgres"));
+});
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,6 +19,12 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+if (app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
